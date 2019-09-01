1e1f5f7018ddc63e8d4cc25cd4d12dea3e64a0ca
teunbrand <tahvdbrand@gmail.com>
2019-09-01 19:19:03 +0200

    Added scale transformations tests

--- tests/testthat/test-facet_grid_sc.R
+++ tests/testthat/test-facet_grid_sc.R
@@ -8,3 +8,26 @@ test_that("facet_grid_sc() accepts custom scales per facet", {
   expect_identical(grid$params$scales$y, sc_y)
 })
 
+test_that("facet_grid_sc() accepts transformations", {
+  sc_y <- list(setosa = ggplot2::scale_y_reverse(),
+               versicolor = ggplot2::scale_y_sqrt(),
+               virginica = ggplot2::scale_y_continuous())
+
+  ctrl <- ggplot2::ggplot(iris, ggplot2::aes(Sepal.Length, Sepal.Width,
+                                             colour = Species)) +
+    ggplot2::geom_point()
+  test <- ctrl + facet_grid_sc(Species ~ ., scales = list(x = "fixed", y = sc_y))
+  ctrl <- ctrl + ggplot2::facet_grid(Species ~ ., scales = "free_y")
+
+  ctrl <- ggplot2::layer_data(ctrl)
+  test <- ggplot2::layer_data(test)
+
+  expect_identical(ctrl[ctrl$PANEL == 1, "y"] * -1,
+                   test[test$PANEL == 1, "y"])
+
+  expect_identical(ctrl[ctrl$PANEL == 2, "y"] ^ 0.5,
+                   test[test$PANEL == 2, "y"])
+
+  expect_identical(ctrl[ctrl$PANEL == 3, "y"],
+                   test[test$PANEL == 3, "y"])
+})
