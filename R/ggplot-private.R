1444d3ed4140d8e8d1883c9137207cd1ce648ca0
Sergio Oller <sergioller@gmail.com>
2022-11-07 11:45:44 +0100

    Show archived message only

--- R/ggplot-private.R
+++
@@ -1,6 +0,0 @@
-# Private functions from ggplot2 used in our package
-# Ideally this file would not exist :-)
-
-check_labeller <- ggplot2:::check_labeller
-
-grid_as_facets_list <- ggplot2:::grid_as_facets_list
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(facetscales)
-
-test_check("facetscales")
